728193ba28b4dc6fc949d10366fa3146c5e087f4
0x00bit <magnus.antonio.j@gmail.com>
2025-09-03 21:18:42 -0300

    server + dockerfile finished

---
+++ app/settings.py
@@ -0,0 +1,21 @@
+import yaml
+import base64
+
+class Setup:
+    """Class for importing settings from a YAML file."""
+
+    def import_settings(self):
+        try:
+            with open('settings.yaml', 'r') as file:
+                settings = yaml.safe_load(file)
+                return settings.get('timeout'), settings.get('hosts', [])
+    
+        except FileNotFoundError:
+            print("Settings file not found.")
+            return 1
+    
+    def getPassword(self, password):
+        if password:
+            decoded_bytes = base64.b64decode(password)
+            return decoded_bytes.decode('utf-8')
+    
