6827cb379e6bff9d38f2211c78bff5b5f3ecebff
Magnus Junior <magnus.antonio.j@gmail.com>
2025-10-25 16:59:40 -0300

    feat: data json recording db

--- app/app.py
+++ app/app.py
@@ -3,10 +3,11 @@ from settings import Setup
 import time
 import threading
 import redis
+import json
 
 class App:
     def __init__(self):
-        self.timeout, self.hosts = Setup().import_settings()
+        self.timeout, self.totens, self.paineis = Setup().import_settings()
         self.conn = redis.Redis(host='redis', port=6379, db=0)
         # Check Redis connection
         try:
@@ -16,20 +17,26 @@ class App:
             print("Redis server is not running. Please start the Redis server and try again.")
             exit(1)
 
-        self.totens = self.hosts['totens']
-        self.panels = self.hosts['paineis']
 
     def isOnline(self, hosts, category):
         for host in hosts:
             command = ["ping", "-c", "3", "-W", "1", host]
             response = subprocess.run(command, capture_output=True, text=True)
             status = 1 if response.returncode == 0 else 0
+            ip = hosts[host][0]
             
             try:
+                data = {
+                    "ip": ip,
+                    "hostname": host,
+                    "status": status,
+                }
+
                 if category == 'painel':
-                    self.conn.hset("PAINEL_STATUS", host, status)
+                    self.conn.hset("PAINEL_STATUS", host, json.dumps(data))
                 elif category == 'totem':
-                    self.conn.hset("TOTEM_STATUS", host, status)
+                    self.conn.hset("TOTEM_STATUS", host, json.dumps(data))
+
             except Exception as a:
                 print(f"Error: it couldn't record on redis db", a)
 
@@ -38,7 +45,7 @@ class App:
             self.conn.expire("PAINEL_STATUS", self.timeout)
             self.conn.expire("TOTEM_STATUS", self.timeout)
             t1 = threading.Thread(target=self.isOnline, args=(self.totens, 'totem'))
-            t2 = threading.Thread(target=self.isOnline, args=(self.panels, 'painel'))
+            t2 = threading.Thread(target=self.isOnline, args=(self.paineis, 'painel'))
             t1.start()
             t2.start()
             t1.join()
--- app/settings.py
+++ app/settings.py
@@ -10,9 +10,7 @@ class Setup:
             path = os.path.join(os.path.dirname(__file__), 'settings.yaml')
             with open(path, 'r') as file:
                 settings = yaml.safe_load(file)
-                return settings.get('timeout'), settings.get('hosts', [])
+                return settings.get('timeout'), settings.get('totens', []), settings.get('paineis', [])
         except FileNotFoundError as e:
             print(f"Settings file not found. {e}")
-            return 1
-
-    
+            return 1, [], []
